585f59e9da24a77b1a12b7256fa58464f630445a
rakesh911 <rkkumar1992a5@gmail.com>
2022-10-19 18:17:12 +0530

    intial commit

---
+++ e2eDemoProject/src/main/java/com/rakesh/demo/e2eDemoProject/App.java
@@ -0,0 +1,13 @@
+package com.rakesh.demo.e2eDemoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ e2eDemoProject/src/test/java/com/rakesh/demo/e2eDemoProject/AppTest.java
@@ -0,0 +1,20 @@
+package com.rakesh.demo.e2eDemoProject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
